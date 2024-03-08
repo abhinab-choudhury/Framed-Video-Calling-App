bb385227cba2bbf91ea2ad12afd5d824ee3148ca
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-03-08 16:52:25 +0530

    Form Component Added
    Broke index page into reproduceable element i.e form componets using props in react

---
+++ src/components/form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function form({ formHeading, labelText, labelFor, imgSize, img, btnText }) {
+  return (
+    <div>
+      <form className="block">
+        <div className="flex flex-row">
+          <div>
+            <h1 className="text-3xl poppins-bold ">{formHeading}</h1>
+            <label className="mx-2 font-bold text-md" htmlFor={labelFor}>{labelText}</label>
+            <input className="block border rounded border-gray-29 h-11 w-4/5 p-3 m-2" id={labelFor} type="text" placeholder={labelFor} />
+            <button className="hover:bg-blue-400 hover:text-white mx-2 font-bold roboto-bold border p-3 rounded" type="submit">{btnText}</button>
+          </div>
+          <img width={imgSize} className="object-fill" src={img} />
+        </div>
+      </form >
+    </div >
+  )
+}
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,4 @@
+import Form from "./form"
 const home = () => {
   return (
     <>
@@ -10,34 +11,12 @@ const home = () => {
         </h1>
         <div className="flex flex-col">
           <div className="m-2 block border rounded-md border-gray-600 justify-center align-middle p-5 w-4/5">
-            <form className="block">
-              <div className="flex flex-row">
-                <div>
-                  <h1 className="text-3xl poppins-bold ">Start a Vidoe Call</h1>
-                  <label className="mx-2 text-md" htmlFor="usename">Name </label>
-                  <input className="block border rounded border-gray-300 h-11 w-4/5 p-3 m-2" id="username" type="text" placeholder="Name" />
-                  <label className="mx-2 text-md" htmlFor="email">Email</label>
-                  <input className="block border rounded border-gray-300 h-11 w-4/5 p-3 m-2" id="email" type="email" placeholder="email" />
-                  <button className="hover:bg-blue-400 hover:text-white mx-2 font-bold roboto-bold border p-3 rounded" type="submit">Start Meeting</button>
-                </div>
-                <img className="object-fill w-72" src="start-meeting.jpg" />
-              </div>
-            </form>
+            <Form formHeading="Start a Video Call" labelText="Meeting Title" labelFor="Title" imgSize="280px" img="start-meeting.jpg" btnText="Start Meeting" />
           </div>
         </div>
         <div className="w-screen flex justify-center align-middle">
           <div className="w-fit m-2 border rounded-md border-gray-600 justify-center align-middle p-5">
-            <form>
-              <div className="flex">
-                <div>
-                  <h1 className="text-3xl poppins-bold ">Join a Video Call</h1>
-                  <label className="mx-2 text-md font-bold" htmlFor="code">Meeting Code</label>
-                  <input className="block border rounded border-gray-300 h-11 p-3 m-2" id="code" type="text" placeholder="Code" />
-                  <button className="hover:bg-blue-400 hover:text-white mx-2 font-bold roboto-bold border p-3 rounded" type="submit">Join Now</button>
-                </div>
-                <img width="480px" className="object-cover" src="join-meeting.jpg" />
-              </div>
-            </form>
+            <Form formHeading="Join a Video Call" labelText="Meeting Code" labelFor="Code" imgSize="480px" img="join-meeting.jpg" btnText="Join Now" />
           </div>
         </div>
       </div>
