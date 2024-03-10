6b13c884eceba0505923af4718aa68080d399aea
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2024-03-10 05:38:49 +0530

    Sigin & Signup page Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
-import Home from './components/home'
-import Navbar from "./components/navbar"
+import Home from './components/Home'
+import Navbar from "./components/Navbar"
+import Sigin from "./components/Signin"
+import Signup from './components/Signup'
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Home />
+      {/* <Navbar /> */}
+      {/* <Home /> */}
+      <Sigin />
+      {/* <Signup /> */}
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Navbar () {
+  return (
+    <nav className="flex w-full fixed top-0 px-9 bg-gray-100 shadow-lg shadow-gray-300/40 z-10">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center">
+          <img width="80" height="80" src="https://img.icons8.com/avantgarde/100/video-message.png" alt="video-message" />
+          <span className="text-3xl font-bold ml-4 poppins-bold"> Framed </span>
+          <img className="h-1/2 w-1/12" src="https://img.icons8.com/ios/100/vertical-line.png" alt="vertical-line" />
+          <span>Crystal clear video calls.</span>
+        </div>
+        <div className="flex items-center">
+          <a href="#" className="text-blue-700 hover:bg-blue-500 hover:text-white block border rounded-md border-blue-400 px-3 py-2">Sign in</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
