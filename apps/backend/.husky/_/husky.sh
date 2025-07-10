4883097c0dc6e2d89cf010d84735379f91502adf
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2025-07-10 12:41:24 +0530

    chore: setup husky pre-commit manually (no deprecated install)

---
+++ apps/backend/.husky/_/husky.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ -z "$husky_skip_init" ]; then
+  debug () {
+    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  export readonly husky_skip_init=1
+  sh -e "$0" "$@"
+  exitCode="$?"
+  debug "completed $hook_name hook"
+  exit "$exitCode"
+fi
